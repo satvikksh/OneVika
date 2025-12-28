445c89ca28db1dd988a1c46604ae9eb0c5e75de2
satvik <satvikksh@gmail.com>
2025-12-28 22:59:18 +0530

    changes

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { getServerSession } from "next-auth";
 import { authOptions } from "../[...nextauth]/route";
 import { NextResponse } from "next/server";
