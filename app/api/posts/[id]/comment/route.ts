24bd08ce445512c265a74baab37c058b2a739240
satvik <satvikksh@gmail.com>
2025-12-29 00:23:22 +0530

    change

--- app/api/posts/[id]/comment/route.ts
+++ app/api/posts/[id]/comment/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../../auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/authOptions";
 import {dbConnect} from "../../../../lib/mongodb";
 import Comment from "../../../../models/Comment";
 import Post from "@/app/models/Post";
