127afe79e9eb793129aac8c6e667160ecddcfa18
satvik <satvikksh@gmail.com>
2026-02-20 16:39:26 +0530

    update

--- app/chat/ChatSidebar.tsx
+++ app/chat/ChatSidebar.tsx
@@ -287,7 +287,7 @@ export default function ChatSidebar({
                             typing...
                           </span>
                         ) : (
-                          user.email || ""
+                          ""
                         )}
                       </p>
                     </div>
